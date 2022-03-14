a786f72fded07eb289baef4d9559ca63de97783b
NabilMustofaa <nabilmustofa6@gmail.com>
2022-03-14 22:11:23 +0700

    tailwind + blade

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,12 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>about</h1>
-</body>
-</html>
+@extends('layouts.main')
+
+
+@section('body')
+<div class="flex flex-col justify-center text-center">
+    <div class='flex justify-center'>
+        <img src="img/foto.jpg" alt="foto nabil" class="flex justify-self-center w-48 border-solid border-2 rounded-full border-black mt-12 ">
+    </div> 
+    <h1 class="text-2xl font-bold mt-4">I' am Nabil Mustofa</h1>
+    <h1 class="text-base text-gray-600">Fullstack Website Developer</h1>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,20 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home',[
+        'title'=> 'Nabil Portofolio'
+    ]);
 });
 Route::get('/about', function () {
-    return view('about');
+    return view('about',[
+        'title'=> 'About Me'
+    ]
+);
+});
+Route::get('/contact', function () {
+    return view('contact',[
+        'title'=> 'Contact'
+    ]
+);
 });
 
